8ea98192e34a05d3a51c8a3d405995cfd9ae77ce
nahiaorgon <nahia_orgon@outlook.com>
2023-05-09 13:00:39 -0300

    vista administracion

---
+++ diseno/Reciplas/Models/Reporte.cs
@@ -0,0 +1,21 @@
+namespace Reciplas.Models;
+
+public class Reporte : EntidadBase{
+
+    public int  ID {get; set;} 
+    public int  dni {get; set;} 
+
+    public string Area {get; set;} 
+    public string Datos {get; set;} 
+    public string nya {get; set;} 
+    public int Telefono {get; set;} 
+    public string Direccion {get; set;} 
+    public string Ciudad {get; set;} 
+    public string Notas {get; set;} 
+    public string Productos {get; set;} 
+    public string Descripcion {get; set;} 
+    public int TotalDebe {get; set;}  
+    public int TotalPagado {get; set;}  
+ 
+        
+}
