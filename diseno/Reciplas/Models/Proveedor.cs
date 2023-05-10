96fea425dca953ea1cdd2638986962dfaf29134c
nahiaorgon <nahia_orgon@outlook.com>
2023-05-10 12:45:59 -0300

    arreglos visuales

--- diseno/Reciplas/Models/Proveedor.cs
+++ diseno/Reciplas/Models/Proveedor.cs
@@ -2,8 +2,10 @@ namespace Reciplas.Models;
 
 public class Proveedor : EntidadBase{
     public int Sucursal {get; set;} 
-    public int Identificacion {get; set;}
+    public int ID {get; set;}
+    public int DNI {get; set;}
     public string Nombre {get; set;}
+    public string Ciudad {get; set;}
     public string Direccion {get; set;}
     
 }
