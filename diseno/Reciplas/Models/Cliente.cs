f60c9df4b2b85357fe46ad5324cfe7704b14da71
Beneyto Mateo <mateobeneyto@gmail.com>
2023-05-20 04:20:52 -0300

    Area de produccion
    Area de produccion

--- diseno/Reciplas/Models/Cliente.cs
+++ diseno/Reciplas/Models/Cliente.cs
@@ -1,7 +1,14 @@
-namespace Reciplas.Models;
 
-public class Cliente : EntidadBase{ 
+namespace Reciplas.Models;
+    public class Cliente : EntidadBase{ 
     public string Nombre {get; set;}
+    public int ID {get; set;}
     public string Direccion {get; set;}
-    public int Dni {get; set;} 
-}
+    public int DNI {get; set;}   
+    public int Telefono {get; set;}   
+    public int Id { get; set; }
+
+    public bool PerfilAprobacion { get; set; }
+
+    }
+
