db50dd96a578e26d65a0e730946f97bc7cbf446b
nahiaorgon <nahia_orgon@outlook.com>
2023-05-22 10:30:02 -0300

    ultimos cambios

---
+++ diseno/Reciplas/Models/OrderDetail.cs
@@ -0,0 +1,8 @@
+namespace Reciplas.Models;
+
+public class OrderDetail : EntidadBase{ 
+    public int OrderID {get; set;}
+    
+    public DateTime OrderDate {get; set;}
+
+}
---
+++ diseno/Reciplas/Models/Servicios.cs
@@ -0,0 +1,15 @@
+/*namespace Empleados.Services;
+
+public class Servicios
+{
+    
+    public Usuario Usuarios { get; }
+    public Area Areas { get; }
+
+    public Servicios(Data.BdContexto WeatherForecastService)
+    {
+        Usuarios = new Usuario(WeatherForecastService);
+
+        Areas = new Area(WeatherForecastService);
+    }
+}*/
